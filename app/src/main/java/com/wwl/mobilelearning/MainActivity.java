e6cbe35cc645b060b4e61f4a6cde3814b5e63f7f
wuyuansushen <wuyuansushen@gmail.com>
2021-12-07 17:34:42 +0800

    Use blank template

---
+++ app/src/main/java/com/wwl/mobilelearning/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wwl.mobilelearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
