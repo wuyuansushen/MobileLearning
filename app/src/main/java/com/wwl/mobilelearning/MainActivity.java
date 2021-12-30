0a3a8ac236bbc3065f164f677a5fb508bbe9275d
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2021-12-31 00:54:19 +0800

    spinner need.

--- app/src/main/java/com/wwl/mobilelearning/MainActivity.java
+++ app/src/main/java/com/wwl/mobilelearning/MainActivity.java
@@ -2,12 +2,14 @@ package com.wwl.mobilelearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -25,21 +27,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         View rootView=getWindow().getDecorView().getRootView();
-        InitialRecyclerView(rootView);
+        InitialData(rootView);
+        UpdateAsset(rootView);
+
+        RecyclerView transactionRecyclerView=this.findViewById(R.id.transactionRecycler);
 
         FloatingActionButton createTransactionButton=this.findViewById(R.id.floatingActionButton);
         createTransactionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(MainActivity.this,CreateTransactions.class);
-
-                //未完成
+                intent.putExtra("positionIndex",transactionList.size());
                 startActivity(intent);
             }
         });
     }
 
-    public void InitialRecyclerView(View view)
+    public void InitialData(View view)
     {
         account=new Account(this.getApplicationContext());
         transactionList=account.loadAccount();
@@ -50,9 +54,23 @@ public class MainActivity extends AppCompatActivity {
         income=0.0;
         expence=0.0;
 
+        TextView assetView=(TextView)view.findViewById(R.id.asset);
+        TextView incomeView=(TextView) view.findViewById(R.id.income);
+        TextView expenceView=(TextView) view.findViewById(R.id.expence);
+
         for(int i=0;i<transactionList.size();i++)
         {
-
+            Double price=transactionList.get(i).getPrice();
+            if(price>0)
+                income=income+price;
+            else
+                expence=expence+price;
         }
+
+        asset=income+expence;
+
+        assetView.setText(Double.toString(asset));
+        incomeView.setText(Double.toString(income));
+        expenceView.setText(Double.toString(expence));
     }
 }
